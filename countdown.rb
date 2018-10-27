ff3e1db3021defadc46617fca5e913016d7b6684
Evan Sturman <evan.sturman@gmail.com>
2018-10-27 20:19:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
 def countdown
+
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+end
+
+"Happy NEW YEAR!"
+end
+
+def countdown_with_sleep
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
